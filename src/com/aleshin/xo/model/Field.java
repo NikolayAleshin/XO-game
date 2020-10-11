ab5399c716b0a6e6a991bfa4589ae19d155b2be3
Nikolay <alstrongnik@gmail.com>
2020-10-11 20:37:11 +0300

    Field - test for the Point added

--- src/com/aleshin/xo/model/Field.java
+++ src/com/aleshin/xo/model/Field.java
@@ -1,9 +1,14 @@
 package com.aleshin.xo.model;
 
+import com.aleshin.xo.model.exceptions.AlreadyOccupiedException;
+import com.aleshin.xo.model.exceptions.InvalidPointException;
+
 import java.awt.*;
 
 public class Field {
-    public static final int FIELD_SIZE = 3;
+    private static final int FIELD_SIZE = 3;
+    private static final int MIN_FIELD_SIZE = 0;
+    private static final int MAX_FIELD_SIZE = FIELD_SIZE;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -11,11 +16,26 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if(!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+        if(field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
+    }
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_FIELD_SIZE && coordinate < MAX_FIELD_SIZE;
     }
 }
