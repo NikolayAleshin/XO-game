ddf850d97c0daf975857e6ddc2da845cac7c3e0b
Nikolay <alstrongnik@gmail.com>
2020-10-11 20:52:23 +0300

    Field checks have been added

--- test/com/aleshin/xo/model/FieldTest.java
+++ test/com/aleshin/xo/model/FieldTest.java
@@ -1,5 +1,7 @@
 package com.aleshin.xo.model;
 
+import com.aleshin.xo.model.exceptions.AlreadyOccupiedException;
+import com.aleshin.xo.model.exceptions.InvalidPointException;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
@@ -9,13 +11,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class FieldTest {
 
     @Test
-    void getFieldSize() {
+    void testGetFieldSize() {
         final Field field = new Field();
         assertEquals(3, field.getFieldSize());
     }
 
     @Test
-    void setFigure() {
+    void testSetFigure() throws InvalidPointException, AlreadyOccupiedException {
         final Field field = new Field();
         final Point point = new Point(0, 0);
         final Figure figure = Figure.O;
@@ -23,5 +25,59 @@ class FieldTest {
         final Figure actualFigure = field.getFigure(point);
 
         assertEquals(figure, actualFigure);
+    }
+
+    @Test
+    void testGetFigureWhenFigureIsNotSet() throws InvalidPointException {
+        final Field field = new Field();
+        final Point point = new Point(0, 0);
+
+        final Figure actualFigure = field.getFigure(point);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    void testGetFigureWhenXIsLessThenZero() throws InvalidPointException, AlreadyOccupiedException {
+        final Field field = new Field();
+        final Point point = new Point(-1, 0);
+
+        try {
+            field.getFigure(point);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void testGetFigureWhenYIsLessThenZero() throws InvalidPointException, AlreadyOccupiedException {
+        final Field field = new Field();
+        final Point point = new Point(0, -1);
+
+        try {
+            field.getFigure(point);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void testGetFigureWhenXIsMoreThenSize() throws InvalidPointException, AlreadyOccupiedException {
+        final Field field = new Field();
+        final Point point = new Point(field.getFieldSize() + 1, 0);
+
+        try {
+            field.getFigure(point);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void testGetFigureWhenYIsMoreThenSize() throws InvalidPointException, AlreadyOccupiedException {
+        final Field field = new Field();
+        final Point point = new Point(0, field.getFieldSize() + 1);
+
+        try {
+            field.getFigure(point);
+            fail();
+        } catch (final InvalidPointException e) {}
     }
 }
